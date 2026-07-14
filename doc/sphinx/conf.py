19c488e45e3bfff72d6ba3d888deee3eccce4627
Stephane Thiell <stephane@thiell.com>
2026-07-14 08:37:06 -0700

    doc: enable sphinx.ext.viewcode
    Signed-off-by: Stephane Thiell <stephane@thiell.com>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -25,7 +25,8 @@ sys.path.insert(0, os.path.abspath('../../lib'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinxcontrib.asciinema']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme',
+              'sphinxcontrib.asciinema']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
